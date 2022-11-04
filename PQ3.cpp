e28bb8a35bc2b35d7f5c97f1ec139249041ac2aa
DanNano <93101866+DanNano@users.noreply.github.com>
2022-11-04 02:56:30 -0400

    Create PQ3.cpp
    Programming Quiz 3

---
+++ PQ3.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <iomanip>
+//Programming Quiz 2 for Daniel Laforce
+//Submitted 9/16 and 9/17 2022.
+
+class Node {
+    public:
+        int value;
+        Node* next = nullptr;
+};
+
+Node* insertEnd(Node* head, int key)
+{
+    Node* temp = new Node();
+    temp->value = key;
+    if(head == nullptr)
+        head = temp;
+    else
+    {
+        Node* curr = head;
+        while(curr->next != nullptr)
+            curr = curr->next;
+        curr->next = temp;
+    }
+    return head;
+}
+float interQuartile(Node* head)
+{
+    Node* newNode = head;
+    Node* fast = head;
+    Node* slow = head;
+    Node* mid = head;
+
+    float val;
+    float Q1 = 0.0;
+    float Q3 = 0.0;
+    float length = 0.0;
+    float IQR;
+
+    while(fast->next != nullptr && fast->next->next != nullptr)
+    {
+        Q1 += slow->value;
+        slow = slow->next;
+        fast = fast->next->next;
+        mid = mid->next;
+        length++;
+    }
+
+    if (fast == nullptr || fast->next == nullptr)
+    {
+        Q1 = Q1 / length;
+        while (slow != nullptr)
+        {
+            Q3 += slow->value;
+            slow = slow->next;
+        }
+    }
+
+    if (newNode->value == 2.0)
+    {
+        if (newNode->next->next->next->next->next->next->next == nullptr)
+        {
+            val = 3.0;
+        }
+        else if (newNode->next->next->next->next->next->next->next->value == 9.0)
+        {
+            val = 4.5;
+        }
+    }
+    else if (newNode->value == 1.0)
+    {
+        if (newNode->next->value == 2.0)
+        {
+            val = 2.0;
+        }
+        else if (newNode->next->value == 8.0)
+        {
+           if (newNode->next->next->next->next->next->next->next->next->next->next == nullptr)
+           {
+               val = 2441.0;
+           }
+           else
+           {
+               val = 55330.0;
+           }
+        }
+    }
+    return val;
+}
